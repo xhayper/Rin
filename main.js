82876c4af545ca8018e01b20cf59dd7b97af9f6c
Glitch (xhayper-rin) <none>
2020-01-18 16:17:07 +0000

    👀🐴 Checkpoint ./main.js:3082634/366

--- main.js
+++ main.js
@@ -180,3 +180,15 @@ Login
 */
 
 client.login(process.env.Bot_Token);
+
+/*
+Config Express
+*/
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello!'))
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
