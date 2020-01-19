f70b0796e61b8e6b2fbe8cefcfd59b795a1838d7
Glitch (xhayper-rin) <none>
2020-01-19 11:33:00 +0000

    🍞🎩 Checkpoint ./Commands/info.js:3082634/1039 ./Commands/help.js:3082634/290 ./main.js:3082634/2

--- Commands/help.js
+++ Commands/help.js
@@ -6,7 +6,13 @@ exports.run = async (client, msg, args, options) => {
         .setColor(9472474)
         .setTimestamp(Date.now())
         .setAuthor("Help menu!", msg.author.avatarURL)
-        .addField("**Music**", `${options.commandList.filter(c => c.config && c.config.category.toLowerCase() === "music").map(c => `**${options.config.prefix}${c.commandName}**`).join("\n")}`, true);
-
+        .addField("**Music**", `${options.commandList.filter(c => c.config && c.config.category.toLowerCase() === "music").map(c => `**${options.config.prefix}${c.commandName}**`).join("\n")}`, true)
+        .addField("**Information**", `${options.commandList.filter(c => c.config && c.config.category.toLowerCase() === "information").map(c => `**${options.config.prefix}${c.commandName}**`).join("\n")}`, true);
+  
     await msg.channel.send(helpEmbed);
+};
+
+exports.config = {
+    category: "information",
+    powerLevel: 0
 };
---
+++ Commands/info.js
@@ -0,0 +1,31 @@
+let Discord = require('discord.js'),
+    os = require("os");
+
+exports.run = async (client, msg, args, options) => {
+
+    let osType = {
+        "Linux": "Linux",
+        "Darwin": "macOS",
+        "Windows_NT": "Windows"
+    };
+
+    let informationEmbed = new Discord.RichEmbed()
+        .setAuthor(`${client.user.username}'s Information!`, client.user.displayAvatarURL)
+        .setColor(9472474)
+        .addField(`Numbers!`,
+            `Guilds : ${client.guilds.size}\n
+            Users : ${client.users.size}\n
+            Channels : ${client.channels.size}\n
+            Commands : ${options.commandList.size} commands\n
+            Playing music on : ${options.queue.length ? options.queue.length : 0} guilds`, true)
+        .addField(`System!`, `
+        OS : ${osType[os.type()]}\n
+        RAM : ${Math.round((os.totalmem() / 1000000) - (os.freemem() / 1000000))}mb/${Math.round(os.totalmem() / 1000000)}mb`, true);
+
+    msg.channel.send(informationEmbed);
+};
+
+exports.config = {
+    category: "information",
+    powerLevel: 0
+};
